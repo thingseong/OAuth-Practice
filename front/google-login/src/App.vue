304fca3c22b7680b89f8b079590e6286d0eb6471
thingseong <audtjd1893@gmail.com>
2024-01-13 22:01:16 +0900

    Vue, Spring Boot 프로젝트 생성

---
+++ front/google-login/src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <div>
+        <button id="google-login"><a href="http://localhost:8080"> Google-login</a></button>
+    </div>
+</template>
+
+<style scoped></style>
+
